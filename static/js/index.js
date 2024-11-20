333bdf56be3467b3025fb6bd234a4586232c73ee
Gustavo <thevicio27@gmail.com>
2024-11-20 14:38:39 -0300

    Adicionado tema escuro, link do LinkedIn adicionado, links corrigidos, cores ajustadas.

---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+function onThemeChange() {
+    const currentTheme = document.documentElement.getAttribute("data-bs-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    document.documentElement.setAttribute("data-bs-theme", newTheme);
+}
