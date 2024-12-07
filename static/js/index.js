4994c0eb647c77ed251fbcf1f6a81b9e0a0b10cd
Gustavo <thevicio27@gmail.com>
2024-12-07 00:47:12 -0300

    feat: O tema é mantido ao recarregar a pagina

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,28 @@
-function onThemeChange() {
-    const currentTheme = document.documentElement.getAttribute("data-bs-theme");
-    const newTheme = currentTheme === "light" ? "dark" : "light";
-    document.documentElement.setAttribute("data-bs-theme", newTheme);
+function getCurrentTheme() {
+    return document.documentElement.getAttribute("data-bs-theme");
 }
+
+function setCurrentTheme(theme) {
+    document.documentElement.setAttribute("data-bs-theme", theme);
+}
+
+function onThemeChange() {
+    const newTheme = getCurrentTheme() === "light" ? "dark" : "light"
+    setCurrentTheme(newTheme);
+    setSavedTheme(newTheme);
+}
+
+function getSavedTheme() {
+    return window.localStorage.getItem("theme");
+}
+
+function setSavedTheme(theme) {
+    window.localStorage.setItem("theme", theme);
+}
+
+function loadTheme() {
+    const savedTheme = getSavedTheme();
+    setCurrentTheme(savedTheme ? savedTheme : "light");
+}
+
+loadTheme();
